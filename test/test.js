cc46fd30d612371b83cf6fa225c68f7fb98eaa71
sabbirahmednirzhor <95560005+sabbirahmednirzhor@users.noreply.github.com>
2021-12-05 14:11:25 +0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ const request = require("supertest");
 const app = require("../index");
 
 
-describe("GET /add/8/8", () => {
-    it("The sum is : 16", (done) => {
-        request(app).get("/add/8/8").expect("The sum is : 16", done);
+describe("GET /add/8/7", () => {
+    it("The sum is : 15", (done) => {
+        request(app).get("/add/8/7").expect("The sum is : 15", done);
     })
 });
 
